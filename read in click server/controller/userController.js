ed8bc2cd8ba4fd99a2ae7605707e4b22650b35b6
ChanaBrenig <shchana63@gmail.com>
2022-05-26 00:24:45 +0300

    user changes

--- read in click server/controller/userController.js
+++ read in click server/controller/userController.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const userSchema = require('../model/userSchema');
 const User = require('../model/userSchema')
-
+var MongoClient = require('mongodb').MongoClient;
+var urlToCreate = "mongodb://localhost/Read-In-Click";//change localhost to srv1 in the seminar
+var url = "mongodb://localhost:27017/";
 const signUp = async (req, res) => {
     let myUser = await new User(req.body.user)
     console.log(myUser);
@@ -17,31 +20,53 @@ const signUp = async (req, res) => {
 }
 
 
-// const jwt = require('jsonwebtoken');
-// const createUser = asy0nc (req, res) => {
-//     console.log("!!!!!");
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     let newUser = await new User(req.body)
-//     await newUser.save(function (err, user) {
-//         if (err)
-//             res.status(400).send(err);
-//         else
 
-//             res.status(200).json({ userCB: user })
-//     })
-// }
 
-const signInById = (req, res) => {
-    console.log("#######signInById########");
-    User.findOne({ userId: req.params.userId })
-        .then((user) => {
-            console.log('its good');
-            res.status(200).json({ theUser: user })
-        })
-        .catch((error) => {
-            res.status(500).json({ error: error })
-        })
+// const signInById = (req, res) => {
+//     console.log("#######signInById########");
+//     User.findOne({ userId: req.params.userId })
+//         .then((user) => {
+//             console.log('its good');
+//              res.status(200).json({ theUser: user }).send();
+        
+//         })
+//         .catch((error) => {
+//             res.status(500).json({ error: error })
+//         })
+//    }
+        
+    
+
+
+const signInById = (req, res)=>{
+    var userId  = req.params.userId;
+    var password = req.params.password;
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("Read-In-Click");
+    var query = {userId, password};
+    dbo.collection("users").find(query).toArray(function (err, result) {
+      if (err) throw err;
+   
+      db.close();
+    
+    console.log( result);
+    return res.json(result);
+    //   } else {
+       // return res.status(401).send();
+     // }
+
+})})
+;
 }
 
 
-module.exports = { signUp, signInById }
+
+
+
+
+
+
+
+
+module.exports = { signUp, signInById}
--- read in click server/route/userRoute.js
+++ read in click server/route/userRoute.js
@@ -5,7 +5,7 @@ const userController = require('../controller/userController');
 route.post('/signUp', userController.signUp)
 //route.post('/createUser', userController.createUser);
 //route.get('/signInById/:userId', userController.signInById);
-route.get('/signInById/:userId', userController.signInById)
+route.get('/signInById/:userId/:password', userController.signInById)
 
 
 module.exports = route;
